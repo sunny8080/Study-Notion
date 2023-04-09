d4ba31cc2fad3d0c6ce47c1a0f865315a2937eab
Sunny Kumar <55615100+sunny8080@users.noreply.github.com>
2023-04-09 21:48:58 +0530

    Add dummy ham burger

--- src/components/LogInTemplate.js
+++ src/components/LogInTemplate.js
@@ -6,10 +6,10 @@ import LogInForm from './LogInForm'
 
 const LogInTemplate = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) => {
     return (
-        <div className='flex justify-between w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0'>
+        <div className='flex flex-col-reverse md:flex-row justify-between w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0'>
 
             {/* left section */}
-            <div className='w-11/12 max-w-[450px]'>
+            <div className='w-11/12  mx-auto mt-12 md:mt-0 md:mx-0  max-w-[450px]'>
                 <h3 className=' text-richblack-5 font-semibold text-3xl leading-[2.375rem]'>{title}</h3>
 
                 <p className='text-lg text-richblack-100 leading-[1.625rem] mt-4'>{desc1}</p>
@@ -23,7 +23,7 @@ const LogInTemplate = ({ title, desc1, desc2, image, formType, setIsLoggedIn }) 
             </div>
 
             {/* right section */}
-            <div className='relative w-11/12 max-w-[450px]'>
+            <div className='relative w-11/12 mx-auto md:mx-0 max-w-[450px]'>
                 <img src={frame} alt="Pattern" width={558} height={504} loading="lazy" />
                 <img src={image} alt="Student" width={558} height={490} loading="lazy"
                     className=' absolute -top-4 right-4'
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-hot-toast';
 import logo from '../assests/Logo.svg'
-
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
 
@@ -14,15 +14,17 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
 
             {/* <Link to={"/"}><img src={require('../assests/Logo.svg').default} width={160} height={32} loading="lazy"  alt="logo" /></Link> */}
 
+
             <Link to={"/"}><img src={logo} width={160} height={32} loading="lazy" alt="logo" /></Link>
 
-            <div className='flex   text-richblack-25 gap-x-6'>
+
+            <div className=' hidden md:flex   text-richblack-25 gap-x-6'>
                 <Link to={'/'} ><h3>Home</h3></Link>
                 <Link to={'/about'}><h3>About</h3></Link>
                 <Link to={'/contact'}><h3>Contact</h3></Link>
             </div>
 
-            <div className='flex items-center gap-x-4'>
+            <div className='hidden md:flex items-center gap-x-4'>
                 {
                     !isLoggedIn &&
                     <Link to={'/login'}><button
@@ -55,6 +57,10 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
                     >Dashboard</button></Link>
                 }
             </div>
+
+            {/* hamburger */}
+            <button class="md:hidden mr-4"><svg stroke="currentColor" fill="#AFB2BF" stroke-width="0" viewBox="0 0 1024 1024" font-size="24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path></svg></button>
+
 
         </nav>
     )
